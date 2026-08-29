d6123951a08a1d6fa3862e051c83be02904850e5
kana <kanade.0210.daisukiyo@gmail.com>
2026-08-29 11:17:36 +0800

    feat: 新增MojangApi 並讓BanCommand與UnbanCommand使用他 chore: reformat code

--- src/main/java/me/bedtwL/oss/api/WebAPI.java
+++ src/main/java/me/bedtwL/oss/api/WebAPI.java
@@ -1,11 +1,11 @@
 package me.bedtwL.oss.api;
 
 import com.sun.net.httpserver.HttpServer;
+
 import java.net.InetSocketAddress;
 
 public class WebAPI {
-    public void startServer(int port)
-    {
+    public void startServer(int port) {
         try {
             HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
             server.createContext("/api/v1", new Handler());
--- src/main/java/me/bedtwL/oss/util/DataUtils.java
+++ src/main/java/me/bedtwL/oss/util/DataUtils.java
@@ -1,4 +1,4 @@
-package me.bedtwL.oss.utils;
+package me.bedtwL.oss.util;
 
 import me.bedtwL.oss.LazyBanOSS;
 
@@ -7,6 +7,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DataUtils {
+    private static final Pattern TIME_PATTERN = Pattern.compile("(\\d+)([smhd])", Pattern.CASE_INSENSITIVE);
+
     public static void init() {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -23,9 +25,11 @@ public class DataUtils {
                     "ban_end INTEGER" +
                     ");";
             stmt.execute(sql);
-        } catch (Exception ignored) {
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
     public static void saveBan(BanEntry entry) {
         String sql = "INSERT INTO bans (uuid, name, reason, ban_start, ban_end) " +
                 "VALUES (?, ?, ?, ?, ?) " +
@@ -41,9 +45,25 @@ public class DataUtils {
             pstmt.setLong(5, entry.getBanEnd());
             pstmt.executeUpdate();
 
-        } catch (Exception ignored) {
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
+    public static void removeBan(String uuid) {
+        String sql = "DELETE FROM bans WHERE uuid = ?";
+
+        try (Connection conn = DriverManager.getConnection(LazyBanOSS.DB_URL);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, uuid);
+            pstmt.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static BanEntry getBan(String uuid) {
         String sql = "SELECT * FROM bans WHERE uuid = ?;";
         try (Connection conn = DriverManager.getConnection(LazyBanOSS.DB_URL);
@@ -61,11 +81,13 @@ public class DataUtils {
                     );
                 }
             }
-        } catch (Exception ignored) {
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return null;
     }
-    public static Boolean IsBanned(String uuid) {
+
+    public static Boolean isBanned(String uuid) {
         String sql = "SELECT ban_end FROM bans WHERE uuid = ?;";
         try (Connection conn = DriverManager.getConnection(LazyBanOSS.DB_URL);
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -73,17 +95,19 @@ public class DataUtils {
             pstmt.setString(1, uuid);
             try (ResultSet rs = pstmt.executeQuery()) {
                 if (rs.next()) {
-                    long a=rs.getLong("ban_end");
-                    if (a>System.currentTimeMillis()/1000)
+                    long a = rs.getLong("ban_end");
+                    if (a > System.currentTimeMillis() / 1000)
                         return true;
-                    if (a==-1)
+                    if (a == -1)
                         return true;
                 }
             }
-        } catch (Exception ignored) {
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return false;
     }
+
     public static String getBannedReason(String uuid) {
         String sql = "SELECT reason FROM bans WHERE uuid = ?;";
         try (Connection conn = DriverManager.getConnection(LazyBanOSS.DB_URL);
@@ -92,13 +116,13 @@ public class DataUtils {
             pstmt.setString(1, uuid);
             try (ResultSet rs = pstmt.executeQuery()) {
                 if (rs.next())
-                     return LazyBanOSS.config.node("ban-msg").getString().replace("{0}",rs.getString("reason"));
+                    return LazyBanOSS.config.node("ban-msg").getString().replace("{0}", rs.getString("reason"));
             }
-        } catch (Exception ignored) {
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return LazyBanOSS.config.node("ban-msg").getString();
     }
-    private static final Pattern TIME_PATTERN = Pattern.compile("(\\d+)([smhd])", Pattern.CASE_INSENSITIVE);
 
     public static long parseToSeconds(String input) {
         Matcher matcher = TIME_PATTERN.matcher(input);
